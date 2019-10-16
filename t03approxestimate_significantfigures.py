a5e9f6948db7e73ca5ce20357eb4d5476c4a0fe7
Gi Soong Chee <gi.soongchee@dhs.sg>
2019-10-17 03:23:25 +0800

    Create t03approxestimate_significantfigures.py

---
+++ t03approxestimate_significantfigures.py
@@ -0,0 +1,20 @@
+# Craft your question and code your solution ~
+# Contributed by V Lew.  Level of difficulty (year 1s) : Basic to Intermediate
+#
+# Objective -
+# Allow student to apply rules to search strings using string functions
+# and classify and compute the significant figure of a given number.
+# Expect students to use if-then-else and for or while loops to search
+# a string, and converting numbers to string and vice versa.
+#
+# Write Python code to state the number of significant figures of 
+# a given number which is a whole number or contains decimal numbers.
+# 192, 83.76, 3, 4.5
+# 506, 1.099, 3.0021, 70.8001
+# 0.10, 0.500, 41.0320, 6.090
+# 0.021, 0.603, 0.00173, 0.01090
+# 3800, 25000, 103000
+#
+# Extension: Write Python code that generate questions,
+# i.e. numbers and query students about the number of signficant
+# figures of the given number, and give feedback to student's answer.
