f0c72038e8819508818f61cdb108937c559accdc
Gi Soong Chee <gi.soongchee@dhs.sg>
2019-10-16 22:05:10 +0800

    Create t01primeshcflcm_isprime3.py

---
+++ t01primeshcflcm_isprime3.py
@@ -0,0 +1,6 @@
+# Is num a prime number? 
+# A prime number is an integer with exactly 2 different factors, 1 and itself.
+# Consider implementing stop checking at int(num ** 0.5) for efficiency.
+
+num = 61
+
