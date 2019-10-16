20e2009c8475bdef815158cc7dd96fe6c08f6b41
Gi Soong Chee <gi.soongchee@dhs.sg>
2019-10-16 14:15:48 +0800

    Create t01primeshighestcommonfactorlowestcommonmultiple.py

---
+++ t01primeshighestcommonfactorlowestcommonmultiple.py
@@ -0,0 +1,6 @@
+# What are the factors of 18?
+#factor: An integer which when multiplied with another integer, results in the product 18. Hence when 18 is divided by this number, the dividend is an integer and there are no remainders.
+num = 18
+for i in range(1, 19):  
+  if num % i == 0:
+    print(i, end=' ')
