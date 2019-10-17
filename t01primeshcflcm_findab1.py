533ab8ea9e29ba2174400649919fb6a69d7b8db4
zhangsongbo1 <53067792+zhangsongbo1@users.noreply.github.com>
2019-10-17 14:09:22 +0800

    Update t01primeshcflcm_findab1.py

--- t01primeshcflcm_findab1.py
+++ t01primeshcflcm_findab1.py
@@ -1,2 +1,14 @@
 # If a and b are integers such that a * b = 17, find the value of a + b.
 
+num=17
+
+for a in range(1, num+1):
+  if num % a == 0:
+    print(a, end'')
+for b in range(a+1, num+1):
+  if num % b == 0:
+    print(b, end'')
+    answer=a+b
+    print(answer)
+    
+    
